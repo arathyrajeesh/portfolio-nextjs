35ddfa4f4b4d1918010b32fd8b08052baccab356
Arathy <arathyrajeesh2@gmail.com>
2025-10-07 20:16:03 +0530

    navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 
 const font=Inter({
   weight:['100','200','300','400','500','600','700','800','900'],
@@ -18,9 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased bg-[#0d0d1f]`}
-      >
+      <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
+        <ResponsiveNav />
         {children}
       </body>
     </html>
---
+++ constant/constant.ts
@@ -0,0 +1,39 @@
+import { url } from "inspector";
+
+export const NavLinks=[
+    {
+        id:1,
+        url:'#',
+        Label:'Home'
+    },
+    {
+        id:2,
+        url:'#',
+        Label:'Services'
+    },    
+    {
+        id:3,
+        url:'#',
+        Label:'Resume'
+    },    
+    {
+        id:4,
+        url:'#',
+        Label:'Works'
+    },    
+    {
+        id:5,
+        url:'#',
+        Label:'Skills'
+    },    
+    {
+        id:6,
+        url:'#',
+        Label:'Testimonials'
+    },    
+    {
+        id:7,
+        url:'#',
+        Label:'Contact'
+    }
+]
