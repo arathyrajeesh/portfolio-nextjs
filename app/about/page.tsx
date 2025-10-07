2f9d7fa129881755a70a701a49d4f11946de9631
Arathy <arathyrajeesh2@gmail.com>
2025-10-07 19:28:28 +0530

    basic routing

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div className='text-white text-5xl'>
+            About Page
+        </div>
+    )
+}
+
+export default AboutPage
