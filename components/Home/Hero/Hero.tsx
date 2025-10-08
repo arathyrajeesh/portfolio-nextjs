f9a6437fac3566ff571feddef8d257f8ea81375b
Arathy <arathyrajeesh2@gmail.com>
2025-10-08 19:02:21 +0530

    hero section add particle

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,9 +3,11 @@ import Image from 'next/image'
 import React from 'react'
 import { BsArrowBarRight, BsArrowRight } from 'react-icons/bs';
 import Typewriter from 'typewriter-effect'
+import ParticlesHero from './ParticleBackground';
 const Hero = () => {
     return (
         <div className='relative h-screen flex items-center justify-center text-white overflow-hidden flex-col '>
+            <ParticlesHero/>
             <div className='relative z-10 flex flex-col items-center '>
                 <Image src="/images/s1.jpg" 
                 alt='heroimage' 
