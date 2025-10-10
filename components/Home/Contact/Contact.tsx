2aeebbc8b4521240eea37efde4478a8c94de26fb
Arathy <arathyrajeesh2@gmail.com>
2025-10-10 21:13:41 +0530

    skill section added

---
+++ components/Home/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className='pt-16 pb-16 '>
+            <div className='w-[90%] md:w-[80%] lg:w-[70%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 items-center '>
+                <div>
+                    <h1 className='text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200'>Schedule a call with me to see if I can help</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,6 +1,7 @@
+'use client';
 import React from 'react'
 import { SiCss3, SiDjango, SiHtml5, SiJavascript, SiPython, SiReact } from 'react-icons/si'
-
+import Tilt from 'react-parallax-tilt'
 
 const skills = [
     {
@@ -33,8 +34,21 @@ function Skills() {
     return (
         <div className='text-white pt-16 pb-16'>
             <h1 className='text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white'>My 
-                <span className='text-cyan-200'>Skills</span>
+                <span className='text-cyan-300'>Skills</span>
             </h1>
+            <div className='flex flex-wrap justify-center gap-6 mt-16 '>
+                {skills.map((skil)=>{
+                    return (
+                        <Tilt key={skil.name} scale={1.5} transitionSpeed={400}>
+                            <div className='bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center 
+                            justify-center shadow-lg transition hover:scale-105'>
+                                <div className='text-5xl mb-4 text-gray-300 '>{skil.icon}</div>
+                                <p className='text-purple-400 mt-1'>{skil.name}</p>
+                            </div>
+                        </Tilt>
+                    )
+                })}
+            </div>
         </div>
     )
 }
